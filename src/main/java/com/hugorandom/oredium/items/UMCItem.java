b8d095f9310b7d2a50925f281da8eabbc3057ec1
Hugo Samayoa <hugobarca2009@gmail.com>
2022-07-27 21:11:02 -0600

    Arreglo de muchos Json y CUM v2

---
+++ src/main/java/com/hugorandom/oredium/items/UMCItem.java
@@ -0,0 +1,52 @@
+package com.hugorandom.oredium.items;
+
+import com.hugorandom.oredium.inits.SoundsInit;
+import com.hugorandom.oredium.util.ItemGroupTabs;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.ExplosionDamageCalculator;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class UMCItem extends Item {
+
+    public UMCItem() {
+        super(new Properties()
+                .tab(ItemGroupTabs.OREDIUM_ITEMS)
+                .rarity(Rarity.UNCOMMON)
+                .stacksTo(16)
+                .setNoRepair()
+                .fireResistant());
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TranslatableComponent("tooltip.oredium.UMC"));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        BlockPos pos = pPlayer.getOnPos();
+        pLevel.explode((Entity)null, DamageSource.badRespawnPointExplosion(), (ExplosionDamageCalculator)null,
+                (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D,
+                5.0F, false, Explosion.BlockInteraction.DESTROY);
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
+}
