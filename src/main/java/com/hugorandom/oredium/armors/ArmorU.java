524f0ef2ab5c321eeb376ba992f5e232db1f756e
HugoRandom <hugobarca2009@gmail.com>
2023-06-15 00:43:33 -0600

    Nuevos diseños, nuevas mecanicas.

--- src/main/java/com/hugorandom/oredium/armors/ArmorU.java
+++ src/main/java/com/hugorandom/oredium/armors/ArmorU.java
@@ -1,15 +1,23 @@
 package com.hugorandom.oredium.armors;
 
+import com.google.common.collect.ImmutableMap;
+import com.hugorandom.oredium.init.EffectsInit;
 import com.hugorandom.oredium.util.ItemGroupTabs;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
 
+import java.util.Map;
+
 public class ArmorU extends ArmorItem {
+
     public ArmorU(ArmorMaterial pMaterial, EquipmentSlot pSlot) {
         super(pMaterial, pSlot,
                 new Properties()
@@ -18,6 +26,24 @@ public class ArmorU extends ArmorItem {
                         .stacksTo(1)
                         .fireResistant());
     }
+
+    private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_EFFECT =
+            (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
+                    .put(ArmorTier.DISPROSIO_UPGRADED, new MobEffectInstance(
+                            MobEffects.FIRE_RESISTANCE, 200, 0))
+                    .put(ArmorTier.FRANITOLINA_UPGRADED, new MobEffectInstance(
+                            MobEffects.REGENERATION, 200, 1))
+                    .put(ArmorTier.MELITONILA_UPGRADED, new MobEffectInstance(
+                            MobEffects.DAMAGE_BOOST, 200, 1))
+                    .put(ArmorTier.TERIUM_UPGRADED, new MobEffectInstance(
+                            MobEffects.DAMAGE_RESISTANCE, 200, 1))
+                    .put(ArmorTier.ALEZARITA_UPGRADED, new MobEffectInstance(
+                            MobEffects.WATER_BREATHING, 200, 0))
+                    .put(ArmorTier.HALINOX_UPGRADED, new MobEffectInstance(
+                            MobEffects.DIG_SPEED, 200, 1))
+                    .put(ArmorTier.MIGUELITIO_UPGRADED, new MobEffectInstance(
+                            MobEffects.MOVEMENT_SPEED, 200, 1))
+                    .build();
 
     @Override
     public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity wearer) {
@@ -32,5 +58,68 @@ public class ArmorU extends ArmorItem {
     @Override
     public boolean isFireResistant() {
         return true;
+    }
+
+    public void onArmorTick(ItemStack stack, Level world, Player player) {
+        if(!world.isClientSide()) {
+            if(hasFullSuitOfArmorOn(player)) {
+                evaluateArmorEffects(player);
+            }
+        }
+    }
+
+    private void evaluateArmorEffects(Player player) {
+        for (Map.Entry<ArmorMaterial, MobEffectInstance> entry : MATERIAL_EFFECT.entrySet()) {
+            ArmorMaterial mapArmorMaterial = entry.getKey();
+            MobEffectInstance mapStatusEffect = entry.getValue();
+
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffect);
+            }
+        }
+    }
+
+    private void addStatusEffectForMaterial(Player player, ArmorMaterial mapArmorMaterial,
+                                            MobEffectInstance mapStatusEffect) {
+        boolean hasPlayerEffect = player.hasEffect(mapStatusEffect.getEffect());
+
+        if(hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
+            player.addEffect(new MobEffectInstance(mapStatusEffect.getEffect(),
+                    mapStatusEffect.getDuration(), mapStatusEffect.getAmplifier()));
+            // Si cualquier parte de la armadura tiene durabilidad menor al 25%, será inestable.
+            if (player.getInventory().getArmor(0).getUseDuration() <
+                        (player.getInventory().getArmor(0).getMaxDamage() * 0.25) ||
+                    player.getInventory().getArmor(1).getUseDuration() <
+                            (player.getInventory().getArmor(1).getMaxDamage() * 0.25) ||
+                    player.getInventory().getArmor(2).getUseDuration() <
+                            (player.getInventory().getArmor(2).getMaxDamage() * 0.25) ||
+                    player.getInventory().getArmor(3).getUseDuration() <
+                            (player.getInventory().getArmor(3).getMaxDamage() * 0.25)){
+                player.addEffect(new MobEffectInstance(EffectsInit.UNSTABLE.get(), 200, 0));
+            };
+        }
+    }
+
+    private boolean hasFullSuitOfArmorOn(Player player) {
+        ItemStack boots = player.getInventory().getArmor(0);
+        ItemStack leggings = player.getInventory().getArmor(1);
+        ItemStack breastplate = player.getInventory().getArmor(2);
+        ItemStack helmet = player.getInventory().getArmor(3);
+        return !helmet.isEmpty() && !breastplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial material, Player player) {
+        for (ItemStack armorStack: player.getInventory().armor) {
+            if(!(armorStack.getItem() instanceof ArmorItem)) {
+                return false;
+            }
+        }
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmor(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmor(1).getItem());
+        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmor(2).getItem());
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmor(3).getItem());
+        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                leggings.getMaterial() == material && boots.getMaterial() == material;
     }
 }
--- src/main/java/com/hugorandom/oredium/effects/UnstableEffect.java
+++ src/main/java/com/hugorandom/oredium/effects/UnstableEffect.java
@@ -1,15 +1,8 @@
 package com.hugorandom.oredium.effects;
 
-import com.hugorandom.oredium.init.EffectsInit;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-
-import java.util.Objects;
 
 public class UnstableEffect extends MobEffect {
 
@@ -19,31 +12,6 @@ public class UnstableEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
-        if (!pLivingEntity.level.isClientSide()){
-            if (pLivingEntity.hasEffect(EffectsInit.VITAMINIZED.get())) {
-                if(Objects.requireNonNull(pLivingEntity.getEffect(EffectsInit.VITAMINIZED.get())).getAmplifier() == 0){
-                    pLivingEntity.removeEffect(EffectsInit.VITAMINIZED.get());
-                }
-                else{
-                    pLivingEntity.addEffect(new MobEffectInstance(EffectsInit.VITAMINIZED.get(),
-                            Objects.requireNonNull(
-                                    pLivingEntity.getEffect(EffectsInit.VITAMINIZED.get())).getDuration() + 1,
-                            Objects.requireNonNull(
-                                    pLivingEntity.getEffect(EffectsInit.VITAMINIZED.get())).getAmplifier() - 1));
-                }
-                pLivingEntity.removeEffect(EffectsInit.UNSTABLE.get());
-            }
-            else {
-                addAttributeModifier(Attributes.MOVEMENT_SPEED, "91AEAA56-376B-4498-935B-2F7F68070635",
-                        (double) -1.0F, AttributeModifier.Operation.MULTIPLY_TOTAL);
-                addAttributeModifier(Attributes.ATTACK_SPEED, "AF8B6E3F-3328-4C0A-AA36-5BA2BB9DBEF3",
-                        (double)-0.1F, AttributeModifier.Operation.MULTIPLY_TOTAL);
-                float DME = 0;
-                if (pLivingEntity.isInWaterOrRain()) DME  += 0.5F;
-                if (!pLivingEntity.isShiftKeyDown()) DME  += 0.5F;
-                pLivingEntity.hurt(new DamageSource("unstable"), (pAmplifier + 1) + DME);
-            }
-        }
         super.applyEffectTick(pLivingEntity, pAmplifier);
     }
 
