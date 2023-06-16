d32c3b17275aa93da0949f9e504e1c7ca84d5bee
HugoRandom <hugobarca2009@gmail.com>
2023-06-16 17:03:19 -0600

    Intento desesperado de fix

--- src/main/java/com/hugorandom/oredium/items/MortarItem.java
+++ src/main/java/com/hugorandom/oredium/items/MortarItem.java
@@ -2,6 +2,7 @@ package com.hugorandom.oredium.items;
 
 import com.hugorandom.oredium.util.ItemGroupTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 
 public class MortarItem extends Item {
@@ -11,5 +12,20 @@ public class MortarItem extends Item {
                 .rarity(Rarity.COMMON)
                 .stacksTo(1)
                 .durability(durability));
+    }
+
+    @Override
+    public boolean hasContainerItem(ItemStack stack) {
+        return true;
+    }
+    @Override
+    public ItemStack getContainerItem(ItemStack itemStack) {
+        ItemStack returnItem = new ItemStack(itemStack.getItem(), 1, itemStack.getDamageValue());
+        if (itemStack.isItemEnchanted())
+        {
+            NBTTagCompound nbtcompound = itemStack.getTagCompound();
+            returnItem.setTagCompound(nbtcompound);
+        }
+        return returnItem;
     }
 }
