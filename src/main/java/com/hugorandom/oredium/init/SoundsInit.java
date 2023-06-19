7ba210d5644127e94742f812dbacd971d1e3cd8e
HugoRandom <hugobarca2009@gmail.com>
2023-06-18 23:49:55 -0600

    Mas arreglos

--- src/main/java/com/hugorandom/oredium/init/SoundsInit.java
+++ src/main/java/com/hugorandom/oredium/init/SoundsInit.java
@@ -26,7 +26,7 @@ public class SoundsInit {
     // Discos de Musica
     public static final RegistryObject<SoundEvent> DISPROSIO_DISC = registerSoundEvent("disprosio_disc");
     public static final RegistryObject<SoundEvent> FRANITOLINA_DISC = registerSoundEvent("franitolina_disc");
-    public static final RegistryObject<SoundEvent> MELITONILA_DISC = registerSoundEvent("meluna_disc");
+    public static final RegistryObject<SoundEvent> MELITONILA_DISC = registerSoundEvent("melitonila_disc");
     public static final RegistryObject<SoundEvent> TERIUM_DISC = registerSoundEvent("terium_disc");
     public static final RegistryObject<SoundEvent> ALEZARITA_DISC = registerSoundEvent("alezarita_disc");
     public static final RegistryObject<SoundEvent> HALINOX_DISC = registerSoundEvent("halinox_disc");
